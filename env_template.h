3b6845e8bdc11443a059e682424348a2e29e2aff
TheTagRapper <pskkjk201@outlook.com>
2026-08-08 13:53:07 +0100

    feat: Now output track name to stdout

---
+++ env_template.h
@@ -0,0 +1,12 @@
+#ifndef ENV_H
+#define ENV_H
+
+// Add your values as string heres from lastfm
+// When done rename file from "env_template.h" to "env.h"
+
+#define API_KEY 
+#define SHARED_SECRET 
+#define USER 
+#define REQUEST_URL
+
+#endif
--- main.c
+++ main.c
@@ -1,16 +1,64 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <stdlib.h>
+#include <cjson/cJSON.h>
+
 #include "env.h"
 
 size_t got_data(char *buffer, size_t itemsize, size_t no_of_items, void* ignorethis)
 {
 	size_t bytes = itemsize * no_of_items;
 
-	printf("New Chunk (%zu bytes)\n", bytes);
-	printf("Buffer: %s\n", buffer);
 	
+	// Parsing JSON
+	cJSON *json = cJSON_Parse(buffer);
+	if (json == NULL)
+	{
+		const char *error_ptr = cJSON_GetErrorPtr();
+		if (error_ptr != NULL) 
+		{
+			printf("Error: %s  \n", error_ptr);
+		}
+		cJSON_Delete(json);
+		return -1;
+	}
+
+	
+	cJSON *recenttracks = cJSON_GetObjectItemCaseSensitive(json, "recenttracks");
+	if (recenttracks == NULL)
+	{
+		printf("reccenttracks doesnt exist");
+		return -1;
+	}
+
+	cJSON *tracks = cJSON_GetObjectItemCaseSensitive(recenttracks, "track");
+	if (tracks == NULL)
+	{
+		printf("track doesnt exist");
+		return -1;
+	}
+
+	cJSON *index = cJSON_GetArrayItem(tracks, 0);
+	if (index == NULL)
+	{
+		printf("0 doesnt exist");
+		return -1;
+	}
+
+	cJSON *name = cJSON_GetObjectItemCaseSensitive(index, "name");
+	if (name == NULL)
+	{
+		printf("track doesnt exist");
+		return -1;
+	}
+		
+	if (cJSON_IsString(name) && (name->valuestring != NULL))
+	{
+		printf("Name: %s \n", name->valuestring);
+	} else printf("FAILURE? \n");
+	cJSON_Delete(json);
 	return bytes;
+
 }
 
 
@@ -31,7 +79,7 @@ int main(void)
 	curl_easy_setopt(curl, CURLOPT_URL, REQUEST_URL);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data); // Call function
 
-	
+
 	// Doing the request
 	result = curl_easy_perform(curl);
 
