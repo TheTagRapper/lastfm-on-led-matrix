49b9c394326a922e68cdec2d85a28ab3e8ce5e26
TheTagRapper <pskkjk201@outlook.com>
2026-09-04 19:01:49 +0100

    Connected the collected JSON to the Parser to display Now Playing info

--- main.c
+++ main.c
@@ -9,13 +9,14 @@
 #include "lwip/altcp_tcp.h"
 #include "lwip/altcp_tls.h"
 #include "mbedtls/ssl.h"
-#include <cjson/cJSON.h>
+#include "cJSON.h"
 #include "env.h"
 
 static ip_addr_t true_ip;
 static bool done = false;
 static bool success = false;
-static char *full_packet = NULL;
+static char full_packet[2*1500*sizeof(char)] = "";
+static char json_string[2*1500*sizeof(char)] = "";
 
 typedef struct TLS_CLIENT_T {
 	struct altcp_pcb *pcb;
@@ -159,7 +160,8 @@ static err_t tls_client_connected(void *arg, struct altcp_pcb *pcb, err_t err)
 		printf("error writing data, err=%d", err);
 		return tls_client_close(state);
 	}
-	printf("Enqueued Request: %s\n", state->http_request);
+	printf("Enqueued Request");
+	//printf(": %s\n", state->http_request);
 
 	
 	err = altcp_output(state->pcb);
@@ -200,6 +202,7 @@ static TLS_CLIENT_T* tls_client_init(void) {
 
 
 static err_t tls_client_poll(void *arg, struct altcp_pcb *pcb) {
+	printf("\nPOLL\n");
 	TLS_CLIENT_T *state = (TLS_CLIENT_T*)arg;
 
 	if (state->connected) return ERR_OK;
@@ -223,7 +226,42 @@ static err_t tls_client_recv(void *arg, struct altcp_pcb *pcb, struct pbuf *p, e
 	// pbuf is packet and needs to be freed later
 	if (!p)
 	{
-		printf("Connection Closed\n");
+		printf("\nConnection Closed\n");
+		//printf("Whole Message: \n %s \n \n", full_packet);
+
+		int size_message = strlen(full_packet);
+		int json_index = 0;
+		if (size_message > 0)
+		{
+			//printf("\nNow Trying to Find JSON string\n");
+			bool json_started = false;
+							
+			for (int i = 0; i < size_message; i++)
+			{
+				char starter_char = '{';
+				//printf("%c", full_packet[i]);
+				if (!json_started && full_packet[i] == '{')
+				{
+					json_started = true;
+					json_string[json_index++] = full_packet[i];
+				}
+				else if (json_started)
+				{
+					json_string[json_index++] = full_packet[i];
+				}
+				
+			}
+
+			printf("Now Parsing JSON STRING : \n %s \n", json_string);
+
+			parse_json_buffer(json_string, sizeof(char), strlen(json_string), NULL);
+			
+			// Reset Static Strings
+			full_packet[0] = '\0';
+			json_string[0] = '\0';
+		}
+		
+		
 		return tls_client_close(state);
 	}
 
@@ -237,11 +275,12 @@ static err_t tls_client_recv(void *arg, struct altcp_pcb *pcb, struct pbuf *p, e
 		pbuf_copy_partial(p, buf, p->tot_len, 0);
 		buf[p->tot_len] = 0;
 
-		printf("\nNew Packet Received:\n %s, \n ", buf);
+		//printf("\nNew Packet Received\n %s, \n ", buf);
 
-		// Concatenate into one string
-		if (full_packet == NULL) full_packet = buff;
-		else strcat(full_packet, buff);
+		//Concatenate into one string
+		if (strlen(full_packet) == 0) strcat(full_packet, buf);
+		else strcat(full_packet, buf);
+		//printf("CONCATENATION OCCURRED\n");
 		
 		// Confirms we have processed the data
 		altcp_recved(pcb, p->tot_len);
@@ -276,7 +315,7 @@ static TLS_CLIENT_T* tls_client_setup()
 	altcp_arg(state->pcb, state);
 	altcp_recv(state->pcb, tls_client_recv);
 	altcp_err(state->pcb, tls_client_err);
-	altcp_poll(state->pcb, tls_client_poll, 20 * 2);
+	altcp_poll(state->pcb, tls_client_poll, 5*2);
 	mbedtls_ssl_set_hostname(altcp_tls_context(state->pcb), "ws.audioscrobbler.com");
 
 	altcp_connect(state->pcb, &true_ip, 443, tls_client_connected);
@@ -286,7 +325,61 @@ static TLS_CLIENT_T* tls_client_setup()
 
 int main(void)
 {
+	// Initiate input
+	stdio_init_all();
+	sleep_ms(2000);
+
+	printf("I/O Initialized\n");
+	// Initiate cyw43
+	cyw43_arch_init();
+
+	// Enable Wifi Mode
+	cyw43_arch_enable_sta_mode();
+	printf("Wifi Enabled\n");
+	// Try to connect to wifi
+	if (cyw43_arch_wifi_connect_timeout_ms(SSID, PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000))
+	{
+		printf("Failed to connect to WiFi\n");
+		fail_state();
+	}
+
+	// Connection sucessful - Find IP
+	cyw43_arch_lwip_begin();
+	err_t err = dns_gethostbyname("ws.audioscrobbler.com", &true_ip, my_dns_found_callback, NULL);
+	cyw43_arch_lwip_end();
+
+	// Keep going until IP is resolved
+	if (err == ERR_INPROGRESS)
+	{
+		while (!done)
+		{
+			cyw43_arch_poll();
+			sleep_ms(10);
+		}
+		// IP resolved
+		if (success)
+		{
+			printf("Resolved to: %s\n", ipaddr_ntoa(&true_ip));
+			// No Certificate
+			TLS_CLIENT_T* state = tls_client_setup();
+			
+			while (!state->complete)
+			{
+				cyw43_arch_poll();
+				cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
+
+			}
+
+		
+		}
 	
+	}
+	else if (err == ERR_OK)
+	{
+		printf("Found %s\n", ipaddr_ntoa(&true_ip));
+	}
+
+	while (true) sleep_ms(1000);		
 
 	
 }
