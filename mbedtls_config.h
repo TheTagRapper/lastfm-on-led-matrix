0bee5a269ddeb4e10a97fbbc1918d2c0571e1c43
TheTagRapper <pskkjk201@outlook.com>
2026-09-20 18:11:16 +0100

    Removed duplicate files

--- mbedtls_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef MBEDTLS_CONFIG_TLS_CLIENT_H
-#define MBEDTLS_CONFIG_TLS_CLIENT_H
-#define MBEDTLS_SSL_MAX_CONTENT_LEN 16384
-#define MBEDTLS_SSL_MAX_FRAGMENT_LENGTH
-
-#include "mbedtls_config_examples_common.h"
-
-#endif
